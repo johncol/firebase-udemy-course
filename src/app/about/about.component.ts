161c0ccf3a57736834a94cd4b8ac8cff48c2f50e
John <john.19col@gmail.com>
2019-09-20 17:07:57 -0500

    Remove sample queries using firestore SDK

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,4 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
-
-import { FirebaseAppConfig } from './../firebase-config';
-import { Course } from '../model/course';
-
-firebase.initializeApp(FirebaseAppConfig);
-
-const db = firebase.firestore();
 
 @Component({
   selector: 'about',
@@ -17,36 +8,5 @@ const db = firebase.firestore();
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {
-    this.fetchCoursesCollection();
-  }
-
-  fetchCourseDocument = () => {
-    db.doc('courses/DiR1bPgMNPKvtHvEaKG2')
-      .get()
-      .then(snapshot => {
-        console.log(`snapshot: `, snapshot);
-        return snapshot.data();
-      })
-      .then(course => {
-        console.log(`course: `, course);
-      })
-      .catch(console.warn);
-  }
-
-  fetchCoursesCollection = () => {
-    db.collection('courses')
-      .get()
-      .then(snapshot => {
-        console.log(`snapshot: `, snapshot);
-        return snapshot.docs.map(courseSnapshot => ({
-          id: courseSnapshot.id,
-          ...courseSnapshot.data()
-        }));
-      })
-      .then((courses: Course[]) => {
-        console.log(`courses: `, courses);
-      })
-      .catch(console.warn);
-  }
+  ngOnInit() {}
 }
--- src/app/firebase-config.ts
+++
@@ -1,9 +0,0 @@
-export const FirebaseAppConfig = {
-  apiKey: 'AIzaSyDF4oGePbAXpsOJzKHGeZJgvWD9v83CweQ',
-  authDomain: 'fir-course-e6097.firebaseapp.com',
-  databaseURL: 'https://fir-course-e6097.firebaseio.com',
-  projectId: 'fir-course-e6097',
-  storageBucket: 'fir-course-e6097.appspot.com',
-  messagingSenderId: '49505601849',
-  appId: '1:49505601849:web:42389eb94397ae6fc300e8'
-};
