d393d12d049cd1f1e94ab37bc7cd446ae278c9b9
John <john.19col@gmail.com>
2019-09-21 16:49:32 -0500

    Update theme and courses cards styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { CoursesService } from '../services/courses.service';
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   public courses$: Observable<Course[]>;
