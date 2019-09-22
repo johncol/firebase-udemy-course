84739cec55005b3e62518d33e667db2e7efb19d9
John <john.19col@gmail.com>
2019-09-22 18:33:34 -0500

    Show loader when fetching courses

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, Subscription } from 'rxjs';
+import { tap, finalize } from 'rxjs/operators';
 
 import { Course } from './../model/course';
 import { Lesson } from './../model/lesson';
@@ -14,9 +14,10 @@ import { CoursesService } from './../services/courses.service';
 })
 export class CourseComponent implements OnInit {
   course: Course;
-  lessons: Lesson[];
-  lessonsPage: number;
-  hasMoreLessons: boolean;
+  lessons: Lesson[] = [];
+  lessonsPage: number = -1;
+  hasMoreLessons: boolean = true;
+  loading: boolean = false;
   displayedColumns = ['seqNo', 'description', 'duration'];
 
   constructor(
@@ -26,26 +27,22 @@ export class CourseComponent implements OnInit {
 
   ngOnInit() {
     this.course = this.route.snapshot.data['course'];
-    this.coursesService
-      .fetchLessons(this.course)
-      .subscribe((lessons: Lesson[]) => {
-        this.lessons = lessons;
-        this.lessonsPage = 0;
-        this.hasMoreLessons = this.thereAreMoreLessons(lessons);
-      });
+    this.fetchCourses();
   }
 
   loadMore() {
+    this.fetchCourses();
+  }
+
+  private fetchCourses = () => {
+    this.loading = true;
     this.coursesService
       .fetchLessons(this.course, this.lessonsPage + 1)
+      .pipe(finalize(() => this.loading = false))
       .subscribe((lessons: Lesson[]) => {
         this.lessons = [...this.lessons, ...lessons];
         this.lessonsPage++;
-        this.hasMoreLessons = this.thereAreMoreLessons(lessons);
+        this.hasMoreLessons = lessons.length == this.coursesService.defaultPageSize;
       });
-  }
-
-  private thereAreMoreLessons = (lessons: Lesson[]) => {
-    return lessons.length == this.coursesService.defaultPageSize;
   }
 }
