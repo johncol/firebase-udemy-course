4ff8869ada2cfd2cc33946ee9cc6023ef24ec9c1
John <john.19col@gmail.com>
2019-09-22 17:31:36 -0500

    Fetch course on path courses/:course-url

--- src/app/services/course.resolver.ts
+++ src/app/services/course.resolver.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Course } from '../model/course';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+
+import { Course } from './../model/course';
+import { CoursesService } from './courses.service';
 
 @Injectable()
 export class CourseResolver implements Resolve<Course> {
-  constructor() {}
+  constructor(private coursesService: CoursesService) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
-    return of({} as Course);
+    const courseUrl: string = route.paramMap.get('courseUrl') as string;
+    return this.coursesService.findOne('url', courseUrl);
   }
 }
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, QueryDocumentSnapshot } from '@angular/fire/firestore';
+import { AngularFirestore, QueryDocumentSnapshot, DocumentChangeAction } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, first } from 'rxjs/operators';
 
 import { Course } from './../model/course';
+
+const COURSES: string = 'courses';
 
 @Injectable({ providedIn: 'root' })
 export class CoursesService {
@@ -11,30 +13,35 @@ export class CoursesService {
 
   public sampleQuery = () => {
     this.db
-      .collection('courses')
+      .collection(COURSES)
       .stateChanges()
       .subscribe(console.log);
   }
 
   public fetchCourses: () => Observable<Course[]> = () => {
     return this.db
-      .collection('courses', collectionRef => {
+      .collection(COURSES, collectionRef => {
         return collectionRef
           .orderBy('seqNo')
           .startAt(0)
           .endAt(100)
       })
       .snapshotChanges()
-      .pipe(
-        map(snaptshots => {
-          return snaptshots
-            .map(snapshot => snapshot.payload.doc)
-            .map(this.docToCourse);
-        })
-      );
+      .pipe(map(this.snapshotsToCourses));
   }
 
-  public filterByCategory(courses: Observable<Course[]>, category: string): Observable<Course[]> {
+  public findOne: (field: string, value: any) => Observable<Course> = (field, value) => {
+    return this.db
+      .collection(COURSES, ref => ref.where(field, "==", value))
+      .snapshotChanges()
+      .pipe(
+        map(this.snapshotsToCourses),
+        map(this.getOneOrEmpty),
+        first()
+      )
+  }
+
+  public filterByCategory: (courses: Observable<Course[]>, category: string) => Observable<Course[]> = (courses, category) => {
     return courses.pipe(
       map(courses => {
         return courses
@@ -43,10 +50,20 @@ export class CoursesService {
     );
   }
 
+  private snapshotsToCourses: (snapshots: DocumentChangeAction<any>[]) => Course[] = snapshots => {
+    return snapshots
+      .map(snapshot => snapshot.payload.doc)
+      .map(this.docToCourse);
+  }
+
   private docToCourse: (doc: QueryDocumentSnapshot<Course>) => Course = doc => {
     return {
       id: doc.id,
       ...doc.data()
     };
+  }
+
+  private getOneOrEmpty: (courses: Course[]) => Course = courses => {
+    return courses.length === 1 ? courses[0] : {} as Course;
   }
 }
