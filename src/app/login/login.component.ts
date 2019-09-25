5921d37cce864cf85d95756c2dbe32521c9b1b55
John <john.19col@gmail.com>
2019-09-25 06:27:42 -0500

    Implement login component using firebase UI

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
 import * as firebaseui from 'firebaseui';
 import * as firebase from 'firebase/app';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent implements OnInit, OnDestroy {
+  ui: firebaseui.auth.AuthUI;
 
-  ngOnInit() {}
+  constructor(
+    private aungularFireAuth: AngularFireAuth,
+    private router: Router,
+    private ngZone: NgZone
+  ) { }
+
+  ngOnInit() {
+    this.ui = new firebaseui.auth.AuthUI(this.aungularFireAuth.auth);
+    this.ui.start('#firebase-ui-auth-container', this.uiConfig())
+  }
+
+  ngOnDestroy() {
+    this.ui.delete();
+  }
+
+  public onLoginSuccessfull = (authResult: any): boolean => {
+    console.log('authResult', authResult);
+    this.ngZone.run(() => this.router.navigate(['courses']));
+    return true;
+  }
+
+  private uiConfig = (): firebaseui.auth.Config => {
+    return {
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      ],
+      callbacks: {
+        signInSuccessWithAuthResult: this.onLoginSuccessfull
+      }
+    };
+  }
 }
