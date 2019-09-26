68fd86f4fd2d6698db5afdffc4e52aab807f1787
John <john.19col@gmail.com>
2019-09-26 17:36:13 -0500

    Add firebase settings for prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDF4oGePbAXpsOJzKHGeZJgvWD9v83CweQ',
+    authDomain: 'fir-course-e6097.firebaseapp.com',
+    databaseURL: 'https://fir-course-e6097.firebaseio.com',
+    projectId: 'fir-course-e6097',
+    storageBucket: 'fir-course-e6097.appspot.com',
+    messagingSenderId: '49505601849',
+    appId: '1:49505601849:web:42389eb94397ae6fc300e8'
+  }
 };
