5b43df7d4d3c44e1dda3fb805a1b2f118503ec8d
John <john.19col@gmail.com>
2019-09-28 13:15:09 -0500

    Add sample hello world http GET function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,8 @@ import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((_request, response) => {
+  response.status(200).json({
+    hello: 'world!'
+  });
+});
